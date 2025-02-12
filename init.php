8d44a56c430bc9a5c5a25c279ec305adf1f22b06
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-12 12:23:39 -0600

    ADD init.php:  initializes constants & variables

---
+++ init.php
@@ -0,0 +1,39 @@
+<?php
+// INITIALIZE VARIABLES
+
+require_once('functions.php');
+
+define('SITE_TITLE', "ElijahStreams Rebuild 2025");
+
+$dummy_vid_title = "Johnny Enlow Unfiltered 138";
+
+// VERSION
+define('VERSION_NUM', '00.01.07');
+
+
+/* COLORS */
+
+$theme_colors = [
+  'oxford_blue' => ($oxford_blue = '#0E1C36'),
+  'robin_egg_blue' => ($robin_egg_blue = '#00CAD0'),
+  'onyx' => ($onyx = '#373943'),
+  'amethyst' => ($amethyst = '#9a6fd7'),
+  'seasalt' => ($seasalt = '#f9fbfe'),
+  'primary' => 'teal',
+  'secondary' => $oxford_blue,
+  
+
+];
+
+
+// __color_primary: teal;
+// __color_secondary: var(__clr_oxford_blue);
+// __color_accent_prim: var(__clr_robin_egg_blue);
+// __color_accent_sec: var(__clr_amethyst);
+// __color_text_base: var(__clr_onyx);
+
+
+
+// [
+//   'primary' => ($primary = )
+// ]

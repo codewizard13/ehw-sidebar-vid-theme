97880c08a061c4e136b6a302bf9c7d8a65e1d314
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-12 12:12:52 -0600

    ADD single.php

---
+++ single.php
@@ -0,0 +1,47 @@
+<!-- HEADER -->
+<?php get_header(); ?>
+
+<!-- BODY -->
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
+
+<!-- SHOW POSTS -->
+<?php
+$custom_query = new WP_Query([
+  'post_type' => ['videos', 'guests'],
+  'posts_per_page' => -1,
+  'post_status' => 'publish',
+  'orderby' => 'date',
+  'order' => 'DESC',
+]);
+
+if ($custom_query->have_posts()): while ($custom_query->have_posts()): $custom_query->the_post(); ?>
+
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+    <header class="entry-header">
+      <h2 class="entry-title">
+        <?php the_title(); ?>
+      </h2>
+    </header>
+    
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div>
+    <hr>
+  
+  </article>
+
+<?php endwhile;
+
+// Reset post data to prevent conflicts
+wp_reset_postdata();
+
+else:
+  echo '<p>No posts found.</p>';
+endif;
+?>
+
+
+<!-- FOOTER -->
+<?php get_footer(); ?>

7cf3b3c2ee4f8f6ae6dd7026cd24fb7a0f745638
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-12 11:13:15 -0600

    ADD index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- HEADER -->
+<?php get_header(); ?>
+
+<!-- BODY -->
+ 
+<!-- SHOW POSTS -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<h1 class="here">Hello World!</h1>
+
+<?php the_content(); ?>
+
+<?php endwhile; else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+<!-- FOOTER -->

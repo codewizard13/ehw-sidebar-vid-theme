307d7a5e3fa96f91102a0e499724c6910f84cbf4
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-12 15:49:06 -0600

    UPDATE init.php:  CHANGE VERSION_NUM

--- init.php
+++ init.php
@@ -8,7 +8,7 @@ define('SITE_TITLE', "ElijahStreams Rebuild 2025");
 $dummy_vid_title = "Johnny Enlow Unfiltered 138";
 
 // VERSION
-define('VERSION_NUM', '00.01.07');
+define('VERSION_NUM', '0.01.00');
 
 
 /* COLORS */

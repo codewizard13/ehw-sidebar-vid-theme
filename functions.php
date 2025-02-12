69c3be07dfd5b4f84f480ba88fcf8b812d70d9c8
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-12 16:01:58 -0600

    UPDATE functions.php:  ENQUEUE jquery & bootstrap js

--- functions.php
+++ functions.php
@@ -28,4 +28,24 @@ function load_css() {
 }
 add_action( 'wp_enqueue_scripts', 'load_css' );
 
+
+function load_js() {
+
+	wp_enqueue_script( 'jquery' );
+
+	wp_register_script( 'bootstrap', get_template_directory_uri() . 'js/bootstrap.min.js', 'jquery', false, true ); // last arg is whether to add to footer
+	wp_enqueue_script( 'bootstrap');
+
+}
+add_action( 'wp_enqueue_scripts', 'load_js' );
+
+
+
+
+
+
+
+
+
+
 // NOTE: The rest of the "plugins" and custom PHP code should be either in the CUSTOM SITE PLUGIN or Code Snippets

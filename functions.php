140fda2bc19941739d5b6afa8fc77a5d8d863cf3
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-12 12:12:28 -0600

    UPDATE functions.php:  ENQUEUE theme styles

--- functions.php
+++ functions.php
@@ -11,18 +11,13 @@ if (!defined('ABSPATH'))
 /**
  * Enqueue styles
  */
-function child_enqueue_styles()
-{
 
-	// 08/27/24
-	// Makes the style.css URL unique after each child theme update
-	//  for CACHE-BUSTING
-	$themeVer = wp_get_theme()->get('Version'). "?bust=" . rand(1001, 9999);
+ function load_theme_files() {
+	
+	wp_enqueue_style( 'elsms_main_styles', get_stylesheet_uri() );
 
+ }
 
-	wp_enqueue_style('elsm-astra-child', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), $themeVer, 'all');
-
-}
-add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
+ add_action( 'wp_enqueue_scripts', 'load_theme_files');
 
 // NOTE: The rest of the "plugins" and custom PHP code should be either in the CUSTOM SITE PLUGIN or Code Snippets

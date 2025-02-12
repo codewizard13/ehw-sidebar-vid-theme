bcfab694c61d3d9c4aa2d59545a51e43a2f6debc
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-12 11:26:06 -0600

    UPDATE index.php:   ADD bloginfo name and tagline

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <?php get_header(); ?>
 
 <!-- BODY -->
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
 
 <!-- SHOW POSTS -->
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>

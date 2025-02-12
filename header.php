2a14682ea24a5534cb960abeae68088d35e85062
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-12 12:58:44 -0600

    UPDATE header.php:  REMOVE all manual properties in HEAD tag

--- header.php
+++ header.php
@@ -4,16 +4,6 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php echo SITE_TITLE ?></title>
-  <link rel="stylesheet" href="css/reset-josh-c.css">
-  <!-- Option 1: Include in HTML -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-  <link rel="stylesheet" href="css/style.css">
-
   <?php wp_head(); ?>
 </head>
 

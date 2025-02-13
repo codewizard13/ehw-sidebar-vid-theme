e7de6389c5f776e48ed816b84d3ef968cebc41a4
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-13 10:58:48 -0600

    UPDATE archive.php:  ADD PAGINATION using paginate_links()

--- archive.php
+++ archive.php
@@ -4,14 +4,17 @@
 
   <?php get_template_part( 'includes/section', 'archive'); ?>
 
-  <?php the_posts_pagination( [ 
-  
-    'mid_size' => 2,
-    'prev_text' => __( 'Previous Page', 'textdomain' ),
-    'next_text' => __( 'Next Page', 'textdomain' ),
-  
-  
-  ]);  
+  <?php 
+    global $wp_query;
+    
+    $big = 999999999; // need an unlikely integer
+    
+    echo paginate_links( array(
+      'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+      'format' => '?paged=%#%',
+      'current' => max( 1, get_query_var('paged') ),
+      'total' => $wp_query->max_num_pages
+    ) );
   ?>
 
 </div>

6b363c2e2e834dd60bc6f5ff3096d01d2070f258
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-13 15:21:01 -0600

    UPDATE single.php:  ADD thumbnail, responsive, fluid

--- single.php
+++ single.php
@@ -3,6 +3,14 @@
 <div class="page-wrap">
   <div class="container">
 
+    <?php if (has_post_thumbnail()): ?>
+
+      THIS HAS A THUMBNAIL
+
+      <img src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>" class="img-fluid">
+
+    <?php endif; ?>
+
     <h1><?php the_title(); ?></h1>
 
     <?php get_template_part('includes/section', 'blogcontent'); ?>

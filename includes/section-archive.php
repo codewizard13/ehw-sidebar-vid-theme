13a9592d3a29170b53a5c17f890bd52ca46b6a8a
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-13 10:33:00 -0600

    UPDATE section-archive.php:  ADD bootstrap styles - link as green button, margin-bottom

--- includes/section-archive.php
+++ includes/section-archive.php
@@ -1,13 +1,13 @@
 <?php if ( have_posts() ): while( have_posts() ): the_post(); ?>
 
-<div class="card">
+<div class="card mb-3">
 
   <div class="card-body">
 
     <h3><?php the_title(); ?></h3>
     <?php the_excerpt(); ?>
 
-    <a href="<?php the_permalink(); ?>">Read more</a>
+    <a href="<?php the_permalink(); ?>" class="btn btn-success">Read more</a>
 
   </div>
 

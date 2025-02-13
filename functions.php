9f4dd8f8db6485eade1b288780505128f4916b41
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-13 08:12:57 -0600

    UPDATE functions.php:  REGISTER nav menus

--- functions.php
+++ functions.php
@@ -15,38 +15,46 @@ if (!defined('ABSPATH'))
  */
 
 // Load Stylesheets
-function load_css() {
+function load_css()
+{
 
-	wp_register_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' , [], false, 'all' );
-	wp_enqueue_style( 'bootstrap' );
+	wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', [], false, 'all');
+	wp_enqueue_style('bootstrap');
 
-	wp_register_style( 'main', get_template_directory_uri() . '/css/main.css' , [], false, 'all' );
-	wp_enqueue_style( 'main' );
+	wp_register_style('main', get_template_directory_uri() . '/css/main.css', [], false, 'all');
+	wp_enqueue_style('main');
 
-	wp_register_style( 'elsms_styles_min', get_template_directory_uri() . '/style.min.css' , [], false, 'all' );
+	wp_register_style('elsms_styles_min', get_template_directory_uri() . '/style.min.css', [], false, 'all');
 	// wp_enqueue_style( 'elsms_styles_min' );
 
 
 
 }
-add_action( 'wp_enqueue_scripts', 'load_css' );
+add_action('wp_enqueue_scripts', 'load_css');
 
 // Load JavaScript
-function load_js() {
+function load_js()
+{
 
-	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script('jquery');
 
-	wp_register_script( 'bootstrap', get_template_directory_uri() . 'js/bootstrap.min.js', 'jquery', false, true ); // last arg is whether to add to footer
-	wp_enqueue_script( 'bootstrap');
+	wp_register_script('bootstrap', get_template_directory_uri() . 'js/bootstrap.min.js', 'jquery', false, true); // last arg is whether to add to footer
+	wp_enqueue_script('bootstrap');
 
 }
-add_action( 'wp_enqueue_scripts', 'load_js' );
+add_action('wp_enqueue_scripts', 'load_js');
 
 
 
 // Theme Options
-add_theme_support( 'menus' );
+add_theme_support('menus');
 
 
 
+// Menus
+register_nav_menus([
 
+	'top-menu' => 'Top Menu Location',
+	'mobile-menu' => 'Mobile Menu Location',
+
+]);

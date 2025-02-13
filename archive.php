323d6588c38edfe619548cb4f0aa54b0eaf58375
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-13 14:46:51 -0600

    MOVE archive pagination to own part file

--- archive.php
+++ archive.php
@@ -7,18 +7,9 @@
 
     <?php get_template_part('includes/section', 'archive'); ?>
 
-    <?php
-    global $wp_query;
+    <?php get_template_part('includes/part', 'pagination-archive'); ?>
 
-    $big = 999999999; // need an unlikely integer
-    
-    echo paginate_links(array(
-      'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-      'format' => '?paged=%#%',
-      'current' => max(1, get_query_var('paged')),
-      'total' => $wp_query->max_num_pages
-    ));
-    ?>
+
 
   </div>
 </section>

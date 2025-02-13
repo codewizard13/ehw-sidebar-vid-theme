4b6c6c7bf6a4200501a7f5fda62fa9d794900933
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-13 12:31:27 -0600

    ADD section-blogcontent.php

---
+++ includes/section-blogcontent.php
@@ -0,0 +1,13 @@
+<?php if ( have_posts() ): while( have_posts() ): the_post(); ?>
+
+  <?php the_content(); ?>
+
+  <?php
+  $fname = get_the_author_meta('first_name');
+  $lname = get_the_author_meta('last_name');
+  ?>
+
+  <p>Posted by <?php echo $fname; ?> <?php echo $lname; ?></p>
+
+
+<?php endwhile; else: endif; ?>

723f4a21aedc002e1661aac7553ffdd8b7d21bca
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-13 08:05:59 -0600

    UPDATE functions.php:  ADD theme support for menus

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 /*
  * functions.php
  * 
+ * NOTE: The rest of the "plugins" and custom PHP code should be either in the CUSTOM SITE PLUGIN or Code Snippets
+ * 
  */
 
 // Exit if accessed directly
@@ -12,8 +14,7 @@ if (!defined('ABSPATH'))
  * Enqueue styles
  */
 
-
-
+// Load Stylesheets
 function load_css() {
 
 	wp_register_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' , [], false, 'all' );
@@ -30,7 +31,7 @@ function load_css() {
 }
 add_action( 'wp_enqueue_scripts', 'load_css' );
 
-
+// Load JavaScript
 function load_js() {
 
 	wp_enqueue_script( 'jquery' );
@@ -43,11 +44,9 @@ add_action( 'wp_enqueue_scripts', 'load_js' );
 
 
 
+// Theme Options
+add_theme_support( 'menus' );
 
 
 
 
-
-
-
-// NOTE: The rest of the "plugins" and custom PHP code should be either in the CUSTOM SITE PLUGIN or Code Snippets

f1a41a161e60d2e1e45eec70a79cd4c1c9eefc82
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-13 14:26:32 -0600

    UPDATE archive.php:  WRAP container in SECTION page-wrap

--- archive.php
+++ archive.php
@@ -1,23 +1,26 @@
 <?php get_header(); ?>
 
-<div class="container">
+<section class="page-wrap">
+  <div class="container">
 
-  <?php get_template_part( 'includes/section', 'archive'); ?>
+    <?php get_template_part('includes/section', 'archive'); ?>
 
-  <?php 
+    <?php
     global $wp_query;
-    
+
     $big = 999999999; // need an unlikely integer
     
-    echo paginate_links( array(
-      'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+    echo paginate_links(array(
+      'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
       'format' => '?paged=%#%',
-      'current' => max( 1, get_query_var('paged') ),
+      'current' => max(1, get_query_var('paged')),
       'total' => $wp_query->max_num_pages
-    ) );
-  ?>
+    ));
+    ?>
 
-</div>
+  </div>
+</section>
+
 
 
 <?php get_footer(); ?>

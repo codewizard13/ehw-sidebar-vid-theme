99332ba1e3b7bf8bc26cf281d2f84258cd704a87
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-14 14:31:51 -0600

    UPDATE functions.php:  ADD image size blog-wide

--- functions.php
+++ functions.php
@@ -65,5 +65,6 @@ register_nav_menus([
 
 
 // Custom Image Sizes
-add_image_size('blog-large', 800, 400, false);
+add_image_size('blog-wide', 1440, 400, true);
+add_image_size('blog-large', 800, 400, true);
 add_image_size('blog-small', 300, 200, true); // last arg is whether to hard-crop

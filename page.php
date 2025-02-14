9a93f44ce02a5840e0ffe15fb316ce2aadcbe6c9
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-14 15:39:30 -0600

    UPDATE page.php:  ADD bootstrap styles for left sidebar

--- page.php
+++ page.php
@@ -4,23 +4,36 @@
   <div class="container">
 
 
-    <?php if ( is_active_sidebar('page-sidebar')): ?>
-
-      <?php dynamic_sidebar('page-sidebar'); ?>
-
-    <?php endif; ?>
+    <section class="row">
 
 
-    <h1><?php the_title(); ?></h1>
+      <div class="col-lg-3">
 
-    <?php if (has_post_thumbnail()): ?>
+        <?php if (is_active_sidebar('page-sidebar')): ?>
 
-      <img src="<?php the_post_thumbnail_url('blog-wide'); ?>" alt="<?php the_title(); ?>"
-        class="img-fluid mb-3 img-thumbnail me-4">
+          <?php dynamic_sidebar('page-sidebar'); ?>
 
-    <?php endif; ?>
+        <?php endif; ?>
 
-    <?php get_template_part('includes/section', 'content'); ?>
+      </div>
+
+
+      <div class="col-lg-9">
+
+        <h1><?php the_title(); ?></h1>
+
+        <?php if (has_post_thumbnail()): ?>
+
+          <img src="<?php the_post_thumbnail_url('blog-wide'); ?>" alt="<?php the_title(); ?>"
+            class="img-fluid mb-3 img-thumbnail me-4">
+
+        <?php endif; ?>
+
+        <?php get_template_part('includes/section', 'content'); ?>
+      </div>
+      
+    </section>
+
 
   </div>
 </div>

9bff3ad5fa37eb9f246859bcc0afc79c5af3c93f
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-14 15:59:42 -0600

    DISABLE category-blog.php; ADD sidebar to archive

--- archive.php
+++ archive.php
@@ -3,11 +3,34 @@
 <section class="page-wrap">
   <div class="container">
 
-    <h1><?php echo single_cat_title(); ?></h1>
+    <section class="row">
 
-    <?php get_template_part('includes/section', 'archive'); ?>
 
-    <?php get_template_part('includes/part', 'pagination-archive'); ?>
+      <div class="col-lg-3">
+
+        <?php if (is_active_sidebar('blog-sidebar')): ?>
+
+          <?php dynamic_sidebar('blog-sidebar'); ?>
+
+        <?php endif; ?>
+
+      </div>
+
+      <div class="col-lg-9">
+
+        <h1><?php echo single_cat_title(); ?></h1>
+
+        <?php get_template_part('includes/section', 'archive'); ?>
+
+        <?php get_template_part('includes/part', 'pagination-archive'); ?>
+
+      </div>
+
+
+    </section>
+
+
+
 
   </div>
 </section>

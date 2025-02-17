c6df641fb68e88268001cb19a27bb8cc6667f594
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-17 11:16:05 -0600

    UPDATE search.php:   ADD get_search_query()

--- search.php
+++ search.php
@@ -4,17 +4,12 @@
   <div class="container">
 
 
-      <div class="col-lg-9">
 
-        <h1><?php echo single_cat_title(); ?></h1>
+        <h1>Search Results for '<?php echo get_search_query(); ?>'</h1>
 
         <?php get_template_part('includes/section', 'archive'); ?>
 
         <?php get_template_part('includes/part', 'pagination-archive'); ?>
-
-      </div>
-
-
 
 
 

0e2a9e9e57e52a4c51ba818f5ce2fc94efefefaf
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-17 16:06:30 -0600

    UPDATE single-cars.php:  REDESIGN layout

--- single-cars.php
+++ single-cars.php
@@ -3,15 +3,33 @@
 <div class="page-wrap">
   <div class="container">
 
+  <h1><?php the_title(); ?></h1>
+
+
     <?php if (has_post_thumbnail()): ?>
 
       <img src="<?php the_post_thumbnail_url('blog-wide');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail">
 
     <?php endif; ?>
 
-    <h1><?php the_title(); ?></h1>
 
-    <?php get_template_part('includes/section', 'blogcontent'); ?>
+    <div class="row">
+
+      <div class="col-lg-6">
+    
+        <?php get_template_part('includes/section', 'blogcontent'); ?>
+        
+      </div>
+
+
+      <div class="col-lg-6">
+
+        
+
+      </div>
+
+    </div>
+
 
   </div>
 </div>

2e9a0228f9d84f7cdb5edf2470fa0cfb3efc4f26
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-17 11:51:18 -0600

    UPDATE searchform.php:  ADD category as hidden value to filter

--- searchform.php
+++ searchform.php
@@ -1,10 +1,19 @@
 <?php
 // Template for the actual search form.
+
+$cats = [
+  'recipes' => 153,
+  'blog' => 149,
+  'cars' => 154
+];
 ?>
 
 
 <form action="/" method="get">
 
+
+  <input type="hidden" name="jim" value="<?php echo $cats['recipes']; ?>">
+  
   <label for="search">Search</label>
   <input type="text" name="s" id="search" value="<?php the_search_query();?>" required>
 

bc2dd6988aebb725a533166e09ea296c649e745c
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-17 16:05:42 -0600

    UPDATE section-blogcontent.php:  WRAP tags display in if

--- includes/section-blogcontent.php
+++ includes/section-blogcontent.php
@@ -15,11 +15,12 @@
   
   <?php  // Add tags
   $tags = get_the_tags();
+  if($tags):
   foreach ($tags as $tag): ?>
 
     <a href="<?php echo get_tag_link($tag->term_id); ?>" class="badge bg-success"><?php echo $tag->name; ?></a>
 
-  <?php endforeach; ?>
+  <?php endforeach; endif; ?>
 
 
 

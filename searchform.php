55d8c85e253e0ab30b0cc7ac74dcc5616e4b3afa
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-17 11:11:05 -0600

    ADD searchform.php:   OVERRIDES default search form

---
+++ searchform.php
@@ -0,0 +1,12 @@
+<?php
+// Template for the actual search form.
+?>
+
+
+<form action="/" method="get">
+
+  <label for="search">Search</label>
+  <input type="text" name="s" id="search" value="<?php the_search_query();?>" required>
+
+  <button type="submit">Search</button>
+</form>

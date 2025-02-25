f35c5ce71406e9e4228b491fc4aad7d460aa4c6a
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-25 15:34:29 -0600

    UPDATE webpack.mix.js:  REMOVE semicolon

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,7 +5,7 @@ let mix = require('laravel-mix');
 mix
    .options({
       // processCssUrls: false // disables rewriting URLs and copying into dist/images/
-   });
+   })
    .js('src/app.js', 'dist')
    .sass('src/app.scss', 'dist')
   //  .setPublicPath('dist');

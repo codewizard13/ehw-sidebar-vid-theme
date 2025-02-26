f370f6bbca18942a0926b6224ebb0ddd215422eb
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-26 15:51:18 -0600

    UPDATE part-latest-cars.php:  CONDITIONAL ADD color, brand

--- includes/part-latest-cars.php
+++ includes/part-latest-cars.php
@@ -9,21 +9,44 @@ print_r($attributes['color']);
 $args = [
 
   'post_type' => 'cars',
-  'meta_key' => 'color', // ACF field
-  'meta_value' => $attributes['color'],
-  'meta_compare' => 'LIKE',
+
+  // 'meta_key' => 'color', // ACF field
+  // 'meta_value' => $attributes['color'],
+  // 'meta_compare' => 'LIKE',
+
   'posts_per_page' => -1, // no limit, no paging
-  'tax_query' => [
+  // 'tax_query' => [
+  //   [
+  //     'taxonomy' => 'brands',
+  //     'field' => 'slug',
+  //     'terms' => [
+  //       $attributes['brand'],
+  //     ]
+  //   ]
+  // ],
+
+];
+
+if (isset($attributes['color'])) {
+
+  $args['meta_key'] = 'color';
+  $args['meta_value'] = $attributes['color'];
+  $args['meta_compare'] = 'LIKE';
+
+}
+
+if (isset($attributes['brand'])) {
+  echo "<h3>Brand: " . $attributes['brand'] . "</h3>";
+
+  $args['tax_query'][] =
     [
       'taxonomy' => 'brands',
       'field' => 'slug',
       'terms' => [
         $attributes['brand'],
       ]
-    ]
-  ],
-
-];
+    ];
+}
 
 $query = new WP_Query($args);
 
